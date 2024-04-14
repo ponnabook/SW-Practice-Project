1c0052c4aaea535ff93a629ba9d445ba95b37d6f
Namprik <wareesa1847@gmail.com>
2024-04-15 06:28:54 +0700

    change response number

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -108,7 +108,7 @@ exports.createReservation = async (req, res, next) => {
     const reservation = await Reservation.create(req.body);
     ReminderEmail.scheduleReminder(req.user.email, req.body);
 
-    res.status(200).json({
+    res.status(201).json({
         success: true,
         msg: "send email reminder to " + req.user.email + " before 1 day of this reservation",
         data: reservation,
