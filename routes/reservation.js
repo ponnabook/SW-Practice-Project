0cf8dddeffca5afbf0a8649acf66f42e4d5c600c
ponnabook <88271077+ponnabook@users.noreply.github.com>
2024-04-17 15:00:24 +0700

    add auth and confirmation email (#3)

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,25 +1,25 @@
 /** @format */
 
-const express = require("express");
+const express = require('express');
 const {
   getReservations,
   getReservation,
   createReservation,
   updateReservation,
   deleteReservation,
-} = require("../controllers/reservations.js");
+} = require('../controllers/reservations.js');
 
 const router = express.Router({ mergeParams: true });
 
-const { protect, authorize } = require("../middleware/auth");
+const { protect, authorize } = require('../middleware/auth');
 
 router
-  .route("/")
+  .route('/')
   .get(protect, getReservations)
-  .post(protect, authorize("admin", "user"), createReservation);
+  .post(protect, authorize('user'), createReservation);
 router
-  .route("/:id")
+  .route('/:id')
   .get(protect, getReservation)
-  .put(protect, authorize("admin", "user"), updateReservation)
-  .delete(protect, authorize("admin", "user"), deleteReservation);
+  .put(protect, authorize('admin', 'user'), updateReservation)
+  .delete(protect, authorize('admin', 'user'), deleteReservation);
 module.exports = router;
